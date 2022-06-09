22f52966d9004398b8c36f650deab3d051dfe1f1
Giuseppe Silletti <giuseppe.silletti@nearform.com>
2022-06-09 20:43:31 +0200

    Emit events only when socket io is linked to the express app

--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -58,7 +58,7 @@ router.post('/:id/upvote', (req, res) => {
 	comments[commentToUpvoteIndex].upvotes =
 		comments[commentToUpvoteIndex].upvotes + 1
 
-	req.app.io.emit('COMMENT_UPVOTED', { commentId: id })
+	req.app.io && req.app.io.emit('COMMENT_UPVOTED', { commentId: id })
 
 	res.sendStatus(200)
 })
